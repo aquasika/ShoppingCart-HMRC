2b5e20d15f2e8e08792c72771286c4c27485c8fa
Abhishek Anand <aby.anand@gmail.com>
2017-04-27 21:51:13 +0100

    Update BasicShoppingCart.scala

--- src/main/scala/hmrc/services/BasicShoppingCart.scala
+++ src/main/scala/hmrc/services/BasicShoppingCart.scala
@@ -9,8 +9,4 @@ class BasicShoppingCart extends Cart {
   override def checkout(selectedItems: List[Product]) = selectedItems.foldLeft(BigDecimal(0)) { (price, product) =>
     product.price + price
   }
-
-  def printProducts(selectedItems: List[Product]) = {
-    selectedItems.foldLeft(BigDecimal("0.0")){(sum, item) =>println(sum); sum + item.price }
-  }
 }
